3b0277f100eae15b055091dad6c49fa2f1289672
JulianMalaval <68005754+JulianMalaval@users.noreply.github.com>
2020-07-27 19:38:52 +1100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,21 +1,68 @@
+function sens () {
+    if (y >= 5) {
+        direction = -1
+    } else if (y <= 1) {
+        direction = 1
+    }
+    if (y1 >= 5) {
+        direction1 = -1
+    } else if (y1 <= 1) {
+        direction1 = 1
+    }
+    if (y2 >= 5) {
+        direction2 = -1
+    } else if (y2 <= 1) {
+        direction2 = 1
+    }
+    if (y3 >= 5) {
+        direction3 = -1
+    } else if (y3 <= 1) {
+        direction3 = 1
+    }
+    if (y4 >= 5) {
+        direction4 = -1
+    } else if (y4 <= 1) {
+        direction4 = 1
+    }
+}
+function mouvement () {
+    for (let index = 0; index <= 2; index++) {
+        led.plotBrightness(0, y - index, liste[index])
+        led.plotBrightness(0, y1 - index, liste[index])
+        led.plotBrightness(0, y2 - index, liste[index])
+        led.plotBrightness(0, y3 - index, liste[index])
+        led.plotBrightness(0, y4 - index, liste[index])
+    }
+    y += direction
+    y1 += direction1
+    y2 += direction2
+    y3 += direction3
+    y4 += direction4
+    basic.pause(200)
+}
+let direction4 = 0
+let direction3 = 0
+let direction2 = 0
+let direction1 = 0
+let direction = 0
+let y4 = 0
+let y3 = 0
+let y2 = 0
+let y1 = 0
 let y = 0
 let liste: number[] = []
-let x = 0
+liste = [0, 255, 0]
+y = 0
+y1 = 1
+y2 = 2
+y3 = 3
+y4 = 4
+direction = 1
+direction1 = 1
+direction2 = 1
+direction3 = 1
+direction4 = 1
 basic.forever(function () {
-    liste = [1, 2, 3, 4]
-    for (let valeur of liste) {
-        led.plot(valeur, valeur)
-        led.plot(x, y)
-        basic.pause(1000)
-        led.unplot(x, y)
-        y += 1
-        if (y == 4) {
-            while (y != 0) {
-                led.plot(x, y)
-                basic.pause(1000)
-                led.unplot(x, y)
-                y += -1
-            }
-        }
-    }
+    mouvement()
+    sens()
 })
